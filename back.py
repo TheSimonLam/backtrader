2168649fbb63403ee14e91343424bed51cb9a088
Simon Lam <thesimonlam@gmail.com>
2022-08-29 22:41:08 +0100

    good lord

--- back.py
+++ back.py
@@ -28,6 +28,12 @@ class TestStrategy(bt.Strategy):
         self.buyprice = None
         self.buycomm = None
 
+        self.isLong = False
+        self.POINT_DISTANCE_TO_CLOSE_TRADE = 0.0099
+        self.STARTING_BET_SIZE = 50
+        self.currentBetSize = 0
+        self.lossStreak = 0
+
         # Add a MovingAverageSimple indicator
         self.sma = bt.indicators.SimpleMovingAverage(
             self.data1, period=self.params.maperiod)
@@ -71,34 +77,25 @@ class TestStrategy(bt.Strategy):
                  (trade.pnl, trade.pnlcomm))
 
     def next(self):
-        # Simply log the closing price of the series from the reference
-        self.log('Close, %.2f' % self.dataclose[0])
+        # self.log('Close, %.2f' % self.dataclose[0])
 
-        # Check if an order is pending ... if yes, we cannot send a 2nd one
         if self.order:
             return
 
-        # Check if we are in the market
         if not self.position:
-
-            # Not yet ... we MIGHT BUY if ...
-            if self.dataclose[0] > self.sma[0]:
-
-                # BUY, BUY, BUY!!! (with all possible default parameters)
+            if self.sma[0] > self.sma[-1]:
                 self.log('BUY CREATE, %.2f' % self.dataclose[0])
-
-                # Keep track of the created order to avoid a 2nd order
-                self.order = self.buy()
-
-        else:
-
-            if self.dataclose[0] < self.sma[0]:
-                # SELL, SELL, SELL!!! (with all possible default parameters)
+                self.buyprice = self.dataclose[0]
+                self.order = self.buy(price=0.1)
+                self.isLong = True
+            else:
                 self.log('SELL CREATE, %.2f' % self.dataclose[0])
-
-                # Keep track of the created order to avoid a 2nd order
-                self.order = self.sell()
-
+                self.lastOrderPrice = self.dataclose[0]
+                self.order = self.sell(price=0.1)
+                self.isLong = False
+        else:
+            if self.buyprice < self.dataclose[0] - self.POINT_DISTANCE_TO_CLOSE_TRADE or self.buyprice < self.dataclose[0] > self.POINT_DISTANCE_TO_CLOSE_TRADE:
+                self.close()
 
 if __name__ == '__main__':
     cerebro = bt.Cerebro()
@@ -129,7 +126,7 @@ if __name__ == '__main__':
     cerebro.resampledata(data, timeframe = bt.TimeFrame.Days, compression = 24)
 
     # Set our desired cash start
-    cerebro.broker.setcash(1000.0)
+    cerebro.broker.setcash(999999999999999.0)
 
     # Add a FixedSize sizer according to the stake
     cerebro.addsizer(bt.sizers.FixedSize, stake=10)
