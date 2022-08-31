bc8d418f9eed6d9bc5f68c70da028d22fb6ade32
Simon Lam <thesimonlam@gmail.com>
2022-08-31 23:06:13 +0100

    progress

--- back1.py
+++ back1.py
@@ -29,6 +29,14 @@ class TestStrategy(bt.Strategy):
         self.buyprice = None
         self.buycomm = None
 
+        self.POINT_DISTANCE_TO_CLOSE_TRADE = 0.015
+        self.BET_SIZE_MULTIPLIER = 24
+
+        self.startingBetSize = 50
+        self.betSize = self.startingBetSize
+        self.shouldLongAccordingTo200MA = False
+        self.isLong = False
+
         # Add a MovingAverageSimple indicator
         self.sma = bt.indicators.SimpleMovingAverage(
             self.data1, period=self.params.maperiod)
@@ -74,6 +82,12 @@ class TestStrategy(bt.Strategy):
     def next(self):
         # Simply log the closing price of the series from the reference
         # self.log('Close, %.2f' % self.dataclose[0])
+        # print(self.dataclose[0])
+
+        if self.sma[0] > self.sma[-1]:
+            self.shouldLongAccordingTo200MA = True
+        elif self.sma[0] < self.sma[-1]:
+            self.shouldLongAccordingTo200MA = False
 
         # Check if an order is pending ... if yes, we cannot send a 2nd one
         if self.order:
@@ -82,23 +96,54 @@ class TestStrategy(bt.Strategy):
         # Check if we are in the market
         if not self.position:
 
-            # Not yet ... we MIGHT BUY if ...
-            if self.dataclose[0] > self.sma[0]:
+            # # Not yet ... we MIGHT BUY if ...
+            # if self.dataclose[0] > self.sma[0]:
 
-                # BUY, BUY, BUY!!! (with all possible default parameters)
-                self.log('BUY CREATE, %.2f' % self.dataclose[0])
+            #     # BUY, BUY, BUY!!! (with all possible default parameters)
+            #     self.log('BUY CREATE, %.2f' % self.dataclose[0])
 
-                # Keep track of the created order to avoid a 2nd order
-                self.order = self.buy()
+            #     # Keep track of the created order to avoid a 2nd order
+            #     self.order = self.buy()
+
+            if not self.position:
+                if self.shouldLongAccordingTo200MA:
+                    self.log('BUY CREATE, %.2f' % self.dataclose[0])
+                    self.buyprice = self.dataclose[0]
+                    self.order = self.buy(price=self.betSize)
+                    self.isLong = True
+                else:
+                    self.log('SELL CREATE, %.2f' % self.dataclose[0])
+                    self.buyprice = self.dataclose[0]
+                    self.order = self.sell(price=self.betSize)
+                    self.isLong = False
 
         else:
-            # Already in the market ... we might sell
-            if len(self) >= (self.bar_executed + 5000):
-                # SELL, SELL, SELL!!! (with all possible default parameters)
-                self.log('SELL CREATE, %.2f' % self.dataclose[0])
+            if self.isLong:
+                if self.buyprice < self.dataclose[0] - self.POINT_DISTANCE_TO_CLOSE_TRADE:
+                    #Good outcome
+                    self.order = self.close()
+                    self.betSize = self.startingBetSize
+                    self.startingBetSize += self.BET_SIZE_MULTIPLIER
+                elif self.buyprice > self.dataclose[0] + self.POINT_DISTANCE_TO_CLOSE_TRADE:
+                    #Bad outcome
+                    self.order = self.close()
+                    self.betSize = self.betSize * 2
+      
+            else: 
+                if self.buyprice > self.dataclose[0] + self.POINT_DISTANCE_TO_CLOSE_TRADE:
+                    #Good outcome
+                    self.order = self.close()
+                    self.betSize = self.startingBetSize
+                    self.startingBetSize += self.BET_SIZE_MULTIPLIER
+                elif self.buyprice < self.dataclose[0] - self.POINT_DISTANCE_TO_CLOSE_TRADE:
+                    #Bad outcome
+                    self.order = self.close()
+                    self.betSize = self.betSize * 2
+                
+            # if len(self) >= (self.bar_executed + 5000):
+            #     self.log('SELL CREATE, %.2f' % self.dataclose[0])
 
-                # Keep track of the created order to avoid a 2nd order
-                self.order = self.close()
+            #     self.order = self.close()
 
 
 if __name__ == '__main__':
@@ -118,7 +163,8 @@ if __name__ == '__main__':
         dataname=datapath,
         dtformat=('%Y-%m-%d %H:%M'),
         fromdate=datetime.datetime(2007, 1, 1),
-        todate=datetime.datetime(2022, 8, 29),
+        todate=datetime.datetime(2007, 10, 20),
+        # todate=datetime.datetime(2022, 8, 29),
         reverse=False,
         nullvalue=0.0,
         datetime=0,
