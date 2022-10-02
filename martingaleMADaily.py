eae145f535dbcc88c7c014d4f1e13bbe9089deb7
Simon Lam <thesimonlam@gmail.com>
2022-10-02 23:48:17 +0100

    y

--- martingaleMADaily.py
+++ martingaleMADaily.py
@@ -30,7 +30,7 @@ class TestStrategy(bt.Strategy):
         self.order = None
         self.buyprice = None
 
-        self.POINT_DISTANCE_TO_CLOSE_TRADE_TP = 0.04
+        self.POINT_DISTANCE_TO_CLOSE_TRADE_TP = 0.01
         self.POINT_DISTANCE_TO_CLOSE_TRADE_SL = 0.07
         self.BET_SIZE_MULTIPLIER = 1
         self.bankrupt = False
